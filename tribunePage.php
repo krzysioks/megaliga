e2398306de40015a5498c3231585d756e7288d9f
Krzysztof <38760633+krzysioks@users.noreply.github.com>
2022-03-01 23:53:19 +0100

    Add dashboard template

---
+++ tribunePage.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Template Name: Dashboard
+Description: Render megaliga dashboard front page: Tribune, table, score of last round
+ */
+
+get_header();
+
+do_action('hestia_before_single_page_wrapper');
+
+?>
+
+<div class="<?php echo hestia_layout(); ?>">
+    <?php
+    $class_to_add = '';
+    if (class_exists('WooCommerce', false) && !is_cart()) {
+        $class_to_add = 'blog-post-wrapper';
+    }
+    ?>
+    <div class="blog-post <?php esc_attr($class_to_add); ?>">
+        <div class="container">
+            <?php
+            if (have_posts()) :
+                while (have_posts()) :
+                    the_post();
+
+                    $sidebar_layout = apply_filters('hestia_sidebar_layout', get_theme_mod('hestia_page_sidebar_layout', 'full-width'));
+                    $wrap_class     = apply_filters('hestia_filter_page_content_classes', 'col-md-8 page-content-wrap ');
+            ?>
+
+                    <article id="post-<?php the_ID(); ?>" class="section section-text">
+                        <div class="row">
+                            <?php
+                            if ($sidebar_layout === 'sidebar-left') {
+                                do_action('hestia_page_sidebar');
+                            }
+                            ?>
+                            <div class="<?php echo esc_attr($wrap_class); ?>">
+                                <?php
+                                do_action('hestia_before_page_content');
+
+                                //custom code starts here
+
+                                //custom code ends here
+
+                                echo apply_filters('hestia_filter_blog_social_icons', '');
+
+                                if (comments_open() || get_comments_number()) :
+                                    comments_template();
+                                endif;
+                                ?>
+                            </div>
+                            <?php
+                            if ($sidebar_layout === 'sidebar-right') {
+                                do_action('hestia_page_sidebar');
+                            }
+                            ?>
+                        </div>
+                    </article>
+            <?php
+                    if (is_paged()) {
+                        hestia_single_pagination();
+                    }
+
+                endwhile;
+            else :
+                get_template_part('template-parts/content', 'none');
+            endif;
+            ?>
+        </div>
+    </div>
+
+    <?php get_footer(); ?>
