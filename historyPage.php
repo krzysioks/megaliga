f0a2cabfe712f5280e9f66af1bdcf7ceb4a7942a
Krzysztof <38760633+krzysioks@users.noreply.github.com>
2022-03-12 01:40:52 +0100

    Adjust styling for history page; Add medals for winners

--- historyPage.php
+++ historyPage.php
@@ -37,117 +37,117 @@ do_action('hestia_before_single_page_wrapper');
                                 do_action('hestia_page_sidebar');
                             }
                             ?>
-                            <div class="<?php echo esc_attr($wrap_class); ?>">
-                                <?php
-                                do_action('hestia_before_page_content');
+                            <?php
+                            do_action('hestia_before_page_content');
 
-                                //custom code starts here
+                            //custom code starts here
+                            global $wpdb;
+
+                            function drawStandings($standings, $side, $groupName, $isPlayoff)
+                            {
                                 global $wpdb;
+                                $margin = $side == 'left' ? 'marginRight40' : '';
+                                echo '<table class="scheduleTable marginTop10 ' . $margin . '" border="0">';
 
-                                function drawStandings($standings, $side, $groupName)
-                                {
-                                    global $wpdb;
-                                    $margin = $side == 'left' ? 'marginRight40' : '';
-                                    echo '<table class="scheduleTable marginTop10 ' . $margin . '" border="0">';
-
-                                    if ($groupName != '') {
-                                        echo '  <tr><td colspan="8" class="scheduleTableName textLeft">Grupa ' . ucfirst($groupName) . '</td></tr>';
-                                    }
-
-                                    echo '  <tr>
-                <th class="scheduleHeader textLeft">m</th>
-                <th class="scheduleHeader standingsHeader textLeft">drużyna</th>
-                <th class="scheduleHeader standingsHeader textLeft">m</th>
-                <th class="scheduleHeader standingsHeader textLeft">z</th>
-                <th class="scheduleHeader standingsHeader textLeft">r</th>
-                <th class="scheduleHeader standingsHeader textLeft">p</th>
-                <th class="scheduleHeader standingsHeader textLeft">suma</th>
-                <th class="scheduleHeader standingsHeader textLeft">+/-</th>
-                <th class="scheduleHeader standingsHeader textLeft">pkt</th>
-            </tr>';
-
-                                    $i = 1;
-                                    foreach ($standings as $team) {
-                                        $trClass = $i % 2 == 0 ? 'even' : 'odd';
-                                        echo '<tr class="' . $trClass . '">
-                <td class="scheduleTdImg paddingLeft10">' . $team->place . '</td>
-                <td class="scheduleTd textLeft">' . $team->team_name . '</td>
-                <td class="scheduleTdImg">' . $team->played . '</td>
-                <td class="scheduleTdImg">' . $team->win . '</td>
-                <td class="scheduleTdImg">' . $team->draw . '</td>
-                <td class="scheduleTdImg">' . $team->defeat . '</td>
-                <td class="scheduleTdImg">' . $team->totalScore . '</td>
-                <td class="scheduleTdImg">' . $team->balance . '</td>
-                <td class="scheduleTdImg">' . $team->points . '</td>';
-                                        echo '</tr>';
-
-                                        $i++;
-                                    }
-
-                                    echo '</table>';
+                                if ($groupName != '') {
+                                    echo '  <tr><td colspan="8" class="scheduleTableName textLeft">Grupa ' . ucfirst($groupName) . '</td></tr>';
                                 }
 
-                                $getSeasonIdToShow = $wpdb->get_results('SELECT id_season, season_name, number_of_groups FROM megaliga_season WHERE current = 0');
+                                echo '  <tr>
+                                            <th class="scheduleHeader textLeft">m</th>
+                                            <th class="scheduleHeader standingsHeader textLeft">drużyna</th>
+                                            <th class="scheduleHeader standingsHeader textLeft">m</th>
+                                            <th class="scheduleHeader standingsHeader textLeft">z</th>
+                                            <th class="scheduleHeader standingsHeader textLeft">r</th>
+                                            <th class="scheduleHeader standingsHeader textLeft">p</th>
+                                            <th class="scheduleHeader standingsHeader textLeft">suma</th>
+                                            <th class="scheduleHeader standingsHeader textLeft">+/-</th>
+                                            <th class="scheduleHeader standingsHeader textLeft">pkt</th>
+                                        </tr>';
 
-                                //content of the history page
-                                echo '<div>';
-                                foreach ($getSeasonIdToShow as $season) {
-                                    echo '<div class="marginBottom20">';
-                                    echo '  <div class="marginBottom20">';
-                                    echo '      <span class="teamOverviewTeamName">sezon: ' . $season->season_name . '</span>';
-                                    echo '  </div>';
-                                    echo '  <div>';
-                                    echo '      <span class="roundName">runda: zasadnicza</span>';
-                                    echo '  </div>';
-                                    echo '  <div class="scheduleContainer">';
-                                    //HARDCODED PART - need change if in the future groups names will change, or its number will change
-                                    //if 2 groups in season present -> names are dolce, gabbana; if 1 group -> dolce&gabbana
-                                    //get data for regular season and draw it
-                                    if ($season->number_of_groups == 2) {
-                                        $standingsDolce = $wpdb->get_results('SELECT place, team_name, played, win, draw, defeat, totalScore, balance, points  FROM megaliga_history WHERE id_season = ' . $season->id_season . ' AND ligue_group = "dolce" AND table_type = "regular" ORDER BY place');
-                                        $standingsGabbana = $wpdb->get_results('SELECT place, team_name, played, win, draw, defeat, totalScore, balance, points  FROM megaliga_history WHERE id_season = ' . $season->id_season . ' AND ligue_group = "gabbana" AND table_type = "regular" ORDER BY place');
+                                $i = 1;
+                                foreach ($standings as $team) {
+                                    $trClass = $i % 2 == 0 ? 'even' : 'odd';
+                                    $medal = ($i == 1 || $i == 2 || $i == 3) && $isPlayoff ? '<img class="marginLeft1em" src="http://megaliga.eu/wp-content/uploads/2022/03/medal' . $i . '.png">' : '';
 
-                                        drawStandings($standingsDolce, 'left', 'dolce');
-                                        drawStandings($standingsGabbana, 'right', 'gabbana');
-                                    } else {
-                                        $standings = $wpdb->get_results('SELECT place, team_name, played, win, draw, defeat, totalScore, balance, points  FROM megaliga_history WHERE id_season = ' . $season->id_season . ' AND ligue_group = "dolce&gabbana" AND table_type = "regular" ORDER BY place');
+                                    echo '<tr class="' . $trClass . '">
+                                            <td class="scheduleTdImg paddingLeft10">' . $team->place . '</td>
+                                            <td class="scheduleTd textLeft">' . $team->team_name . $medal . '</td>
+                                            <td class="scheduleTdImg">' . $team->played . '</td>
+                                            <td class="scheduleTdImg">' . $team->win . '</td>
+                                            <td class="scheduleTdImg">' . $team->draw . '</td>
+                                            <td class="scheduleTdImg">' . $team->defeat . '</td>
+                                            <td class="scheduleTdImg">' . $team->totalScore . '</td>
+                                            <td class="scheduleTdImg">' . $team->balance . '</td>
+                                            <td class="scheduleTdImg">' . $team->points . '</td>';
+                                    echo '</tr>';
 
-                                        drawStandings($standings, 'left', 'Dolce&Gabbana');
-                                    }
+                                    $i++;
+                                }
 
-                                    echo '  </div>';
+                                echo '</table>';
+                            }
 
-                                    //get playoff data
-                                    $playoffStandings = $wpdb->get_results('SELECT place, team_name, played, win, draw, defeat, totalScore, balance, points  FROM megaliga_history WHERE id_season = ' . $season->id_season . ' AND table_type = "playoff" ORDER BY place');
+                            $getSeasonIdToShow = $wpdb->get_results('SELECT id_season, season_name, number_of_groups FROM megaliga_season WHERE current = 0 ORDER BY season_name DESC');
 
-                                    //draw playoff data
-                                    if (count($playoffStandings) != 0) {
-                                        echo '<div>';
-                                        echo '  <span class="roundName">runda: playoff</span>';
-                                        echo '</div>';
-                                        echo '  <div>';
-                                        drawStandings($playoffStandings, 'left', '');
-                                        echo '</div>';
-                                    }
+                            //content of the history page
+                            echo '<div>';
+                            foreach ($getSeasonIdToShow as $season) {
+                                echo '<div class="marginBottom20">';
+                                echo '  <div class="marginBottom20 marginLeft1em">';
+                                echo '      <span class="teamOverviewTeamName">sezon: ' . $season->season_name . '</span>';
+                                echo '  </div>';
+                                echo '  <div>';
+                                echo '      <span class="roundName marginLeft1em">runda: zasadnicza</span>';
+                                echo '  </div>';
+                                echo '  <div class="scheduleContainer marginBottom2em historyTableContainer">';
+                                //HARDCODED PART - need change if in the future groups names will change, or its number will change
+                                //if 2 groups in season present -> names are dolce, gabbana; if 1 group -> dolce&gabbana
+                                //get data for regular season and draw it
+                                if ($season->number_of_groups == 2) {
+                                    $standingsDolce = $wpdb->get_results('SELECT place, team_name, played, win, draw, defeat, totalScore, balance, points  FROM megaliga_history WHERE id_season = ' . $season->id_season . ' AND ligue_group = "dolce" AND table_type = "regular" ORDER BY place');
+                                    $standingsGabbana = $wpdb->get_results('SELECT place, team_name, played, win, draw, defeat, totalScore, balance, points  FROM megaliga_history WHERE id_season = ' . $season->id_season . ' AND ligue_group = "gabbana" AND table_type = "regular" ORDER BY place');
+
+                                    drawStandings($standingsDolce, 'left', 'dolce', false);
+                                    drawStandings($standingsGabbana, 'right', 'gabbana', false);
+                                } else {
+                                    $standings = $wpdb->get_results('SELECT place, team_name, played, win, draw, defeat, totalScore, balance, points  FROM megaliga_history WHERE id_season = ' . $season->id_season . ' AND ligue_group = "dolce&gabbana" AND table_type = "regular" ORDER BY place');
+
+                                    drawStandings($standings, 'none', 'Dolce&Gabbana', false);
+                                }
+
+                                echo '  </div>';
+
+                                //get playoff data
+                                $playoffStandings = $wpdb->get_results('SELECT place, team_name, played, win, draw, defeat, totalScore, balance, points  FROM megaliga_history WHERE id_season = ' . $season->id_season . ' AND table_type = "playoff" ORDER BY place');
+
+                                //draw playoff data
+                                if (count($playoffStandings) != 0) {
+                                    echo '<div class="marginLeft1em">';
+                                    echo '  <span class="roundName">runda: playoff</span>';
+                                    echo '</div>';
+                                    echo '  <div class="historyTableContainer">';
+                                    drawStandings($playoffStandings, 'none', '', true);
                                     echo '</div>';
                                 }
-
                                 echo '</div>';
-                                //custom code ends here
-
-                                echo apply_filters('hestia_filter_blog_social_icons', '');
-
-                                if (comments_open() || get_comments_number()) :
-                                    comments_template();
-                                endif;
-                                ?>
-                            </div>
-                            <?php
-                            if ($sidebar_layout === 'sidebar-right') {
-                                do_action('hestia_page_sidebar');
                             }
+
+                            echo '</div>';
+                            //custom code ends here
+
+                            echo apply_filters('hestia_filter_blog_social_icons', '');
+
+                            if (comments_open() || get_comments_number()) :
+                                comments_template();
+                            endif;
                             ?>
                         </div>
+                        <?php
+                        if ($sidebar_layout === 'sidebar-right') {
+                            do_action('hestia_page_sidebar');
+                        }
+                        ?>
                     </article>
             <?php
                     if (is_paged()) {
