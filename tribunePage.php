704bf9524d6aa0a3ebda01ccf593a1c1c02eab1c
Krzysztof <38760633+krzysioks@users.noreply.github.com>
2022-03-14 23:36:57 +0100

    Implement dashboard  - Updated megaliga configuration Docs  - Added setting current round number when closing selecting roster

--- tribunePage.php
+++ tribunePage.php
@@ -35,21 +35,137 @@ do_action('hestia_before_single_page_wrapper');
                                 do_action('hestia_page_sidebar');
                             }
                             ?>
-                            <div class="<?php echo esc_attr($wrap_class); ?>">
-                                <?php
-                                do_action('hestia_before_page_content');
 
-                                //custom code starts here
+                            <?php
+                            do_action('hestia_before_page_content');
 
-                                //custom code ends here
+                            function drawChampion($championData, $title)
+                            {
+                                echo '  <div class="playoffPhaseTitlePosition marginTop20"><span class="playoffPhaseTitle">' . $title . '</span></div>';
+                                echo '  <div class="winnerContainerDashboard">';
+                                echo '      <div class="winnerImgContainer displayFlex">';
+                                echo '          <img class="winner" src="' . $championData->logo_url . '" width="75px" height="75px">';
+                                echo '      </div>';
+                                echo '      <div class="winnerNameContainer displayFlex center">';
+                                echo '        <span class="winnerName">' . $championData->team_name . '</span>';
+                                echo '      </div>';
+                                echo '  </div>';
+                            }
 
-                                echo apply_filters('hestia_filter_blog_social_icons', '');
+                            function drawCurrentRoundScore($queryTeam1Result, $queryTeam2Result, $gameIdentificationData, $groupName, $side, $round_number)
+                            {
+                                $margin = $side == 'left' ? 'marginRight40' : '';
+                                echo '<table class="megaligaScoresTable scheduleTable ' . $margin . '" border="0">';
+                                echo '  <tr><td colspan="6" class="scheduleTableName textLeft">Grupa ' . $groupName . '</td></tr>';
+                                echo '  <tr>
+                            <th colspan="3" class="scheduleHeader textLeft">megaliga</th>
+                            <th colspan="3" class="scheduleHeader textRight">' . $round_number . '. kolejka</th>
+                        </tr>';
+                                $i = 0;
+                                foreach ($gameIdentificationData as $gameIdentity) {
+                                    //take team1 and team2 records that represents the same game in schedule
+                                    $team1Data = array();
+                                    $team2Data = array();
+                                    foreach ($queryTeam1Result as $field) {
+                                        if ($field->id_user_team1 == $gameIdentity->id_user_team1) {
+                                            $team1Data = $field;
+                                        }
+                                    }
+                                    foreach ($queryTeam2Result as $field) {
+                                        if ($field->id_user_team2 == $gameIdentity->id_user_team2) {
+                                            $team2Data = $field;
+                                        }
+                                    }
 
-                                if (comments_open() || get_comments_number()) :
-                                    comments_template();
-                                endif;
-                                ?>
-                            </div>
+                                    $trClass = $i % 2 == 0 ? 'even' : 'odd';
+                                    echo '<tr class="' . $trClass . '">
+                            <td class="scheduleTd textLeft">' . $team1Data->team_name . '</td>
+                            <td class="scheduleTdImg">
+                                <img src="' . $team1Data->logo_url . '" width="40px" height="40px" class="floatRight">
+                            </td>';
+                                    $score1 = $team1Data->team1_score ? $team1Data->team1_score : '-';
+                                    $score1 = strlen($score1) === 2 ? $score1 . '&nbsp;' : $score1;
+                                    $score2 = $team2Data->team2_score ? $team2Data->team2_score : '-';
+                                    $scheduleLeft = $team1Data->team1_score ? '' : 'scheduleNoScoreLeft';
+                                    echo '<td class="scheduleTd scheduleScore ' . $scheduleLeft . ' floatRight">' . $score1 . ' :</td>
+                            <td class="scheduleTd scheduleScore scheduleScoreRight floatLeft">' . $score2 . '</td>';
+
+                                    echo '  <td class="scheduleTdImg">
+                                <img src="' . $team2Data->logo_url . '" width="40px" height="40px" class="floatLeft">
+                            </td>
+                            <td class="scheduleTd textRight">' . $team2Data->team_name . '</td>
+                        </tr>';
+
+                                    $i++;
+                                }
+
+                                echo '</table>';
+                            }
+
+                            $getRound = $wpdb->get_results('SELECT current_round FROM megaliga_current_round WHERE id_current_round = 1');
+
+                            //get teams for Dolce ligue
+                            $getSchedule4DolceTeam1 = $wpdb->get_results('SELECT megaliga_team_names.name as "team_name", megaliga_schedule.team1_score, megaliga_schedule.id_user_team1, megaliga_user_data.logo_url FROM megaliga_user_data, megaliga_team_names, megaliga_schedule WHERE megaliga_user_data.ID = megaliga_schedule.id_user_team1 AND megaliga_user_data.ligue_groups_id = megaliga_schedule.id_ligue_group AND megaliga_schedule.id_ligue_group = 1 AND megaliga_user_data.team_names_id = megaliga_team_names.team_names_id AND megaliga_schedule.round_number = ' . $getRound[0]->current_round);
+
+                            $getSchedule4DolceTeam2 = $wpdb->get_results('SELECT megaliga_team_names.name as "team_name", megaliga_schedule.team2_score, megaliga_schedule.id_user_team2, megaliga_user_data.logo_url FROM megaliga_user_data, megaliga_team_names, megaliga_schedule WHERE megaliga_user_data.ID = megaliga_schedule.id_user_team2 AND megaliga_user_data.ligue_groups_id = megaliga_schedule.id_ligue_group AND megaliga_schedule.id_ligue_group = 1 AND megaliga_user_data.team_names_id = megaliga_team_names.team_names_id AND megaliga_schedule.round_number = ' . $getRound[0]->current_round);
+
+                            //get teams for Gabbama ligue
+                            $getSchedule4GabbanaTeam1 = $wpdb->get_results('SELECT megaliga_team_names.name as "team_name", megaliga_schedule.team1_score, megaliga_schedule.id_user_team1, megaliga_user_data.logo_url FROM megaliga_user_data, megaliga_team_names, megaliga_schedule WHERE megaliga_user_data.ID = megaliga_schedule.id_user_team1 AND megaliga_user_data.ligue_groups_id = megaliga_schedule.id_ligue_group AND megaliga_schedule.id_ligue_group = 2 AND megaliga_user_data.team_names_id = megaliga_team_names.team_names_id AND megaliga_schedule.round_number = ' . $getRound[0]->current_round);
+
+                            $getSchedule4GabbanaTeam2 = $wpdb->get_results('SELECT megaliga_team_names.name as "team_name", megaliga_schedule.team2_score, megaliga_schedule.id_user_team2, megaliga_user_data.logo_url FROM megaliga_user_data, megaliga_team_names, megaliga_schedule WHERE megaliga_user_data.ID = megaliga_schedule.id_user_team2 AND megaliga_user_data.ligue_groups_id = megaliga_schedule.id_ligue_group AND megaliga_schedule.id_ligue_group = 2 AND megaliga_user_data.team_names_id = megaliga_team_names.team_names_id AND megaliga_schedule.round_number = ' . $getRound[0]->current_round);
+
+                            //get all games for Dolce for given round
+                            $getGames4Dolce = $wpdb->get_results('SELECT id_schedule, id_user_team1, id_user_team2 FROM megaliga_schedule WHERE id_ligue_group = 1 AND round_number = ' . $getRound[0]->current_round);
+
+                            //get all games for Gabbana for given round
+                            $getGames4Gabbana = $wpdb->get_results('SELECT id_schedule, id_user_team1, id_user_team2 FROM megaliga_schedule WHERE id_ligue_group = 2 AND round_number = ' . $getRound[0]->current_round);
+
+                            //get data for current champion
+                            $getChampionData = $wpdb->get_results('SELECT team_name, logo_url FROM megaliga_champion');
+
+                            //get data for previous champions
+                            $getPreviousChampions = $wpdb->get_results('SELECT team_name, logo_url, season_name FROM megaliga_history_champion ORDER BY season_name DESC');
+
+                            //custom code starts here
+                            echo '<div class="displayFlex dashboardContainer">';
+                            echo '  <div class="displayFlex dashboardCol1">';
+                            echo '      <div class="individualCommentsTitle">Komunikaty</div>';
+                            echo '      <div class="individualCommentsContainer">';
+                            the_content();
+                            echo '      </div>';
+                            echo '      <div class="tribuneContainer">';
+
+                            echo apply_filters('hestia_filter_blog_social_icons', '');
+
+                            if (comments_open() || get_comments_number()) :
+                                comments_template();
+                            endif;
+
+                            echo '      </div>';
+                            echo '  </div>';
+                            echo '  <div class="displayFlex dashboardCol2">';
+                            echo '      <div class="currentChampion">';
+                            drawChampion($getChampionData[0], 'Mistrz megaligi');
+                            echo '      </div>';
+                            foreach ($getPreviousChampions as $champion) {
+                                echo '  <div class="previousChampion">';
+                                $title = 'Mistrz megaligi ' . $champion->season_name;
+                                drawChampion($champion, $title);
+                                echo '  </div>';
+                            }
+                            echo '      <div class="individualCommentsTitle">Wyniki ostatniej kolejki</div>';
+                            echo '      <div class="scoreTableDolce">';
+                            drawCurrentRoundScore($getSchedule4DolceTeam1, $getSchedule4DolceTeam2, $getGames4Dolce, 'dolce', 'right', $getRound[0]->current_round);
+                            echo '      </div>';
+                            echo '      <div class="scoreTableGabbana">';
+                            drawCurrentRoundScore($getSchedule4GabbanaTeam1, $getSchedule4GabbanaTeam2, $getGames4Gabbana, 'gabbana', 'right', $getRound[0]->current_round);
+                            echo '      </div>';
+                            echo '  </div>';
+
+                            echo '</div>';
+                            //custom code ends here
+                            ?>
+
                             <?php
                             if ($sidebar_layout === 'sidebar-right') {
                                 do_action('hestia_page_sidebar');
